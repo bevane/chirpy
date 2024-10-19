8f7fc2043b2d95cb7fe5c2808d326cad1fb55057
bevane <bevane@live.com>
2024-10-19 15:08:13 +0700

    feat: add healthz handler

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import "net/http"
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", readyHandler)
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
 	server.ListenAndServe()
+}
+
+func readyHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
