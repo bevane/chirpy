e831c34ffb3d73392df68bebf65c67288117244c
bevane <bevane@live.com>
2024-11-13 14:52:45 +0700

    feat: add validate_chirp endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -15,9 +16,10 @@ func main() {
 	serveMux := http.NewServeMux()
 	fileHandler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
 	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(fileHandler))
-	serveMux.HandleFunc("/healthz", readyHandler)
-	serveMux.HandleFunc("/metrics", apiCfg.metricsHandler)
-	serveMux.HandleFunc("/reset", apiCfg.resetHandler)
+	serveMux.HandleFunc("GET /api/healthz", readyHandler)
+	serveMux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	serveMux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	serveMux.HandleFunc("POST /api/validate_chirp", apiCfg.validateChirpHandler)
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
@@ -39,9 +41,14 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
@@ -49,4 +56,52 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Reset successful"))
+}
+
+func (cfg *apiConfig) validateChirpHandler(w http.ResponseWriter, req *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type returnError struct {
+		Error string `json:"error"`
+	}
+
+	type returnSuccess struct {
+		Valid bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	var respBodyError returnError
+	var respBodySuccess returnSuccess
+	err := decoder.Decode(&params)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(500)
+		respBodyError = returnError{
+			Error: "Something went wrong",
+		}
+		dat, _ := json.Marshal(respBodyError)
+		w.Write(dat)
+		return
+	}
+	if len(params.Body) > 140 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(400)
+		respBodyError = returnError{
+			Error: "Chirp is too long",
+		}
+		dat, _ := json.Marshal(respBodyError)
+		w.Write(dat)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	respBodySuccess = returnSuccess{
+		Valid: true,
+	}
+	dat, _ := json.Marshal(respBodySuccess)
+	w.Write(dat)
+	return
 }
